fb79d03b9d0e41a213e34abd62a9e841b6451992
salman murtaza <salman.murtaza.minhas@outlook.com>
2026-02-02 22:09:38 +0300

    updated for DAB, shifted to a single students array from 2, added spin animation

--- js/script.js
+++ js/script.js
@@ -4,21 +4,17 @@ const startButton = document.querySelector('#startButton')
 const remained = document.querySelector('.remain')
 const nextButton = document.querySelector('.nextButton')
 
-let students1 = [
-  'Amira',
+///////////////////////////////////////////////////////
+// MAKE SURE TO KEEP THE NUMBER OF STUDENTS EVEN NUMBER
+///////////////////////////////////////////////////////
+let students = [
+  'Salman',
   'Bashayer',
   'Hasan',
   'Husain',
-  // 'Kawthar',
   'Majid',
   'Dawood',
-  'Noor',
-  'S.Ebrahim'
-]
-
-let students2 = [
-  // 'Raja',
-  'Michael',
+  'S.Ebrahim',
   'Sami',
   'Saud',
   'S.Ali',
@@ -28,80 +24,92 @@ let students2 = [
   'Zainab S'
 ]
 
-// let studentsarray1 = []
-// let studentsarray2 = []
-
 let s1 = ''
 let s2 = ''
 let dance01 = document.querySelector('.dance1')
 let dance02 = document.querySelector('.dance2')
 let dance03 = document.querySelector('.dance3')
 
-// let mainDiv = document.querySelector('.flex-con')
+const x = document.querySelector('.s1')
+const y = document.querySelector('.s2')
 
-// mainDiv.appendChild(dance01)
-// mainDiv.appendChild(dance02)
-// mainDiv.appendChild(dance03)
+let spinInterval = null
 
 const randm = () => {
-  let index = Math.floor(Math.random() * students1.length)
-  s1 = students1[index]
-  console.log(s1)
-  if(students1.length > 0){
-    remained.innerHTML = (students1.length *2 -2) + ' Remains'
-  }else {
+  if (students.length === 0) {
+    remained.innerHTML = 'Relax bro, the game is over!'
+    x.innerHTML = 'GAME'
+    y.innerHTML = 'OVER'
+    return
+  }
+
+  let index1 = Math.floor(Math.random() * students.length)
+  s1 = students[index1]
+  // console.log(s1)
+  students.splice(index1, 1)
+
+  if (students.length > 0) {
+    let index2 = Math.floor(Math.random() * students.length)
+    s2 = students[index2]
+    // console.log(s2)
+    students.splice(index2, 1)
+  } else {
+    s2 = ''
+  }
+  if (students.length >= 0) {
+    remained.innerHTML = students.length + ' Remains'
+  } else {
     remained.innerHTML = 'Relax bro, the game is over!'
   }
-  students1.splice(index, 1)
-  console.log(students1.length)
-  clickAudio.play()
-  nextButton.disabled = true;
-  x.innerHTML = ''
-  // studentsarray1 = localStorage.setItem('studentsarray1', students1);
-  if(students1.length > 0 || students2.length > 0){
-      setTimeout(() => {
-    x.innerHTML = s1;
-    dance01.setAttribute('src', 'img/dance 1.gif')
-    dance02.setAttribute('src', 'img/dance 2.gif')
-    dance03.setAttribute('src', 'img/dance 4.gif')
-  }, 7700)
 
-  setTimeout(() => {
-    nextButton.disabled = false;
-    dance01.setAttribute('src', '')
-    dance02.setAttribute('src', '')
-    dance03.setAttribute('src', '')
-  }, 36000)
-  
-  randm2()
-  return s1
+  clickAudio.play()
+  nextButton.disabled = true
+  x.innerHTML = ''
+  y.innerHTML = ''
+  clearInterval(spinInterval)
+  spinInterval = setInterval(() => {
+    if (students.length === 0) return
+
+    const a = students[Math.floor(Math.random() * students.length)]
+    let b = a
+    if (students.length > 1) {
+      while (b === a) b = students[Math.floor(Math.random() * students.length)]
+    }
+
+    x.innerHTML = a
+    y.innerHTML = b
+  }, 90)
+
+  console.log('Made By Salman Murtaza')
+
+  if (students.length > 0 || s1 || s2) {
+    setTimeout(() => {
+      clearInterval(spinInterval)
+      x.innerHTML = s1
+      y.innerHTML = s2
+
+      dance01.setAttribute('src', 'img/dance 1.gif')
+      dance02.setAttribute('src', 'img/dance 2.gif')
+      dance03.setAttribute('src', 'img/dance 4.gif')
+    }, 7700)
+
+    setTimeout(() => {
+      nextButton.disabled = false
+      dance01.setAttribute('src', '')
+      dance02.setAttribute('src', '')
+      dance03.setAttribute('src', '')
+    }, 36000)
+
+    return s1
   } else {
     x.innerHTML = 'GAME'
     y.innerHTML = 'OVER'
   }
-
-}
-const randm2 = () => {
-  let index = Math.floor(Math.random() * students2.length)
-  s2 = students2[index]
-  console.log(s2)
-  students2.splice(index, 1)
-  console.log(students2.length)
-  y.innerHTML = ''
-  // studentsarray2 = localStorage.setItem('studentsarray2', students2);
-  setTimeout(() => {y.innerHTML = s2}, 7700)
-  
-  return s2
 }
 
-const playVideo = () => {
-  video.play()
-  setTimeout(() => {window.location.href = 'game.html'}, 25000)
-}
-
-
-const x = document.querySelector('.s1')
-const y = document.querySelector('.s2')
-
-
-
+// const playVideo = () => {
+//   video.play()
+//   setTimeout(() => {
+//     window.location.href = 'game.html'
+//   }, 25000)
+// }
